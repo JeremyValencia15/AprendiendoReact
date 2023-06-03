dc6bd593a713295c79c0d60183d5ca03e977d832
JeremyValencia <104913014+JeremyValencia@users.noreply.github.com>
2023-06-03 13:57:25 -0600

    REACT
    Primer commit al repositorio de react

---
+++ FundamentosJS/js/01.js
@@ -0,0 +1,18 @@
+// Variables - let, const
+
+let cliente = 'Juan' // Iniciar variable y asignarle valor
+let precioDescuento = 220
+
+// Re - asiganar 
+cliente = 20
+cliente = true
+
+// Pueden iniciar sin un valor asignado
+
+let precio 
+
+precio = 1000
+
+console.log(precio)
+console.log(cliente)
+console.log(precioDescuento)
